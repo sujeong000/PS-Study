55b5f47b06fe4bdf7b935b4f5087b415b15ab11e
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2022-02-23 21:06:07 +0900

    Create 20944.cpp

---
+++ BOJ/20000-20999/20944.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> N;
+    
+    for(int i=0; i<N; i++) cout << "a";
+    
+    return 0;
+}
