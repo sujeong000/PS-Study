484eca6191a8593df5e1447c1836ee196c50f7a1
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2022-05-24 20:56:22 +0900

    Create 16172.cpp

---
+++ BOJ/16000-16999/16172.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> getPartialMatch(const string& N) {
+     int m = N.size();
+     vector<int> pi(m, 0);
+
+     int begin = 1, matched = 0;
+     while (begin+matched < m) {
+         if (N[begin+matched] == N[matched]) {
+             matched++;
+             pi[begin+matched-1] = matched;
+         }
+         else {
+             if (matched == 0) begin++;
+             else {
+                 begin += matched - pi[matched-1];
+                 matched = pi[matched-1];
+             }
+         }
+     }
+
+     return pi;
+ }
+
+ int KMP(const string& H, const string& N) {
+     int n = H.size(), m = N.size();
+     vector<int> pi = getPartialMatch(N);
+     int ret = 0;
+
+     int begin = 0, matched = 0;
+     while (begin <= n-m) {
+         if (matched < m && H[begin+matched] == N[matched]) {
+             matched++;
+             if (matched == m) ret++;
+         }
+         else {
+             if (matched == 0) begin++;
+             else {
+                 begin += matched - pi[matched-1];
+                 matched = pi[matched-1];
+             }
+         }
+     }
+
+     return ret;
+ }
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    string S, K, SS = "";
+    
+    cin >> S >> K;
+    for (int i=0; i<S.size(); i++) {
+        if ('0' <= S[i] && S[i] <= '9') continue;
+        SS += S[i];
+    }
+    
+    if (KMP(SS, K)) cout << 1;
+    else cout << 0;
+
+    return 0;
+}
