288e7f4081eeb36e6b23efa002bf5114bcdd43d0
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2022-05-28 19:01:40 +0900

    Create 16975.cpp

---
+++ BOJ/16000-16999/16975.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+
+struct Segtree {
+    int n;
+    vector<ll> tree, lazy;
+     
+    Segtree(const vector<ll>& A) {
+        n = A.size();
+        tree.resize(n*4);
+        lazy.resize(n*4);
+        init(A, 0, n-1, 1);
+    }
+    
+    void init(const vector<ll>& A, int left, int right, int node) {
+        if (left == right) {
+            tree[node] = A[left];
+            return;
+        }
+        
+        int mid = (left+right) / 2;
+        init(A, left, mid, node*2);
+        init(A, mid+1, right, node*2+1);
+        tree[node] = tree[node*2] + tree[node*2+1];
+    }
+    
+    void update_lazy(int left, int right, int node) {
+        if (lazy[node] == 0) return;
+        
+        // 켜진 전구가 tree[node]개라면 반전 후에는 (right-left+1) - tree[node]개가 켜져있음
+        tree[node] += lazy[node]*(right-left+1);
+        if (left != right) {
+            lazy[node*2] += lazy[node];
+            lazy[node*2+1] += lazy[node];
+        }
+        lazy[node] = 0;
+    }
+    
+    void update_range(int left, int right, int node, int nodeLeft, int nodeRight, ll val) {
+        update_lazy(nodeLeft, nodeRight, node);
+        
+        if (right < nodeLeft || nodeRight < left) return;
+        if (left <= nodeLeft && nodeRight <= right) {
+            tree[node] += val*(nodeRight-nodeLeft+1);
+            if (nodeLeft != nodeRight) {
+                lazy[node*2] += val;
+                lazy[node*2+1] += val;
+            }
+            return;
+        }
+        
+        int mid = (nodeLeft+nodeRight)/2;
+        update_range(left, right, node*2, nodeLeft, mid, val);
+        update_range(left, right, node*2+1, mid+1, nodeRight, val);
+        tree[node] = tree[node*2] + tree[node*2+1];
+    }
+    
+    void update_range(int left, int right, ll val) {
+        update_range(left, right, 1, 0, n-1, val);
+    }
+    
+    ll query(int left, int right, int node, int nodeLeft, int nodeRight) {
+        update_lazy(nodeLeft, nodeRight, node);
+        
+        if (right < nodeLeft || nodeRight < left) return 0;
+        if (left <= nodeLeft && nodeRight <= right) return tree[node];
+        
+        int mid = (nodeLeft+nodeRight) / 2;
+        ll leftSum = query(left, right, node*2, nodeLeft, mid);
+        ll rightSum = query(left, right, node*2+1, mid+1, nodeRight);
+        return leftSum + rightSum;
+    }
+    
+    ll query(int left, int right) {
+        if (left > right) return 0;
+        else return query(left, right, 1, 0, n-1);
+    }
+};
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N, M;
+    ll a, b, c, d;
+    
+    cin >> N;
+    vector<ll> A(N);
+    for (int i=0; i<N; i++) cin >> A[i];
+    
+    Segtree ST(A);
+    
+    cin >> M;
+    for (int i=0; i<M; i++) {
+        cin >> a;
+        
+        if (a == 1) {
+            cin >> b >> c >> d;
+            ST.update_range(b-1, c-1, d);
+        }
+        else {
+            cin >> b;
+            cout << ST.query(0, b-1) - ST.query(0, b-2) << "\n";
+        }
+    }
+
+    return 0;
+}
