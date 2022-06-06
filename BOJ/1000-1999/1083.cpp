c45f92fd64d2ac20ec671a92c6e63a5cecac0fe1
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2022-06-06 22:09:44 +0900

    Create 1083.cpp

---
+++ BOJ/1000-1999/1083.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N, S;
+int A[51];
+
+int main()
+{
+    cin >> N;
+    for (int i=0; i<N; i++) cin >> A[i];
+    cin >> S;
+    
+    int start = 0;
+    while (start < N && S > 0) {
+        // S번 이내의 스왑으로 끌어올 수 있는 가장 큰 값 끌어오기
+        int maxIdx = start;
+        
+        for (int i=start; i<=min(N-1, start+S); i++) {
+            if (A[maxIdx] < A[i]) maxIdx = i;
+        }
+        for (int i=maxIdx; i>start; i--) swap(A[i], A[i-1]);
+        
+        S -= maxIdx - start;
+        start++;
+    }
+    
+    for (int i=0; i<N; i++) cout << A[i] << " ";
+
+    return 0;
+}
