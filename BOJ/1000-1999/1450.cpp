b6071cd89031b0c579038af5e34512eedc18b902
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2022-07-01 01:10:08 +0900

    Update 1450.cpp

--- BOJ/1000-1999/1450.cpp
+++ BOJ/1000-1999/1450.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <map>
 #include <vector>
 #include <algorithm>
 
@@ -8,20 +7,18 @@ using namespace std;
 typedef long long ll;
 
 int N, C;
-int items[30];
-map<int, int> lMap, rMap;
-vector<int> lItems;
-vector<ll> psum;
+vector<int> items(30);
+vector<int> lCombi, rCombi;
 
-void makeMap(map<int, int>& mp, int idx, int x, int end) {
+void makeCombi(vector<int>& v, int idx, int x, int end) {
     if (x > C) return;
     if (idx == end) {
-        mp[x]++;
+        v.push_back(x);
         return;
     }
 
-    makeMap(mp, idx+1, x+items[idx], end);
-    makeMap(mp, idx+1, x, end);
+    makeCombi(v, idx+1, x+items[idx], end);
+    makeCombi(v, idx+1, x, end);
 }
 
 int main()
@@ -32,21 +29,14 @@ int main()
     cin >> N >> C;
     for (int i=0; i<N; i++) cin >> items[i];
 
-    makeMap(lMap, 0, 0, N/2);
-    makeMap(rMap, N/2, 0, N);
+    makeCombi(lCombi, 0, 0, N/2);
+    makeCombi(rCombi, N/2, 0, N);
     
-    int idx = 0;
-    for (auto iter=lMap.begin(); iter != lMap.end(); iter++) {
-        lItems.push_back(iter->first);
-        if (idx == 0) psum.push_back(iter->second);
-        else psum.push_back(psum.back() + iter->second);
-        idx++;
-    }
+    sort(rCombi.begin(), rCombi.end());
     
     ll ans = 0;
-    for (auto iter=rMap.begin(); iter != rMap.end(); iter++) {
-        int ub = upper_bound(lItems.begin(), lItems.end(), C - iter->first) - lItems.begin();
-        ans += psum[ub-1] * iter->second;
+    for (auto x: lCombi) {
+        ans += upper_bound(rCombi.begin(), rCombi.end(), C-x) - rCombi.begin();
     }
     
     cout << ans;
