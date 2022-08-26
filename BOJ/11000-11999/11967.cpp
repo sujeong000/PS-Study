b5c740e147598c3010679bc319da4e563007dfde
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2022-08-26 19:50:25 +0900

    Update 11967.cpp

--- BOJ/11000-11999/11967.cpp
+++ BOJ/11000-11999/11967.cpp
@@ -5,38 +5,13 @@ using namespace std;
 typedef pair<int, int> pii;
 
 int N, M;
-int parent[10000], sz[10000];
 vector<pii> rooms[100][100];
-
-int find(int x) {
-    if (parent[x] == x) return x;
-    return parent[x] = find(parent[x]);
-}
-
-void merge(int a, int b) {
-    a = find(a); b = find(b);
-    
-    if (a == b) return;
-    if (sz[a] < sz[b]) swap(a, b);
-    
-    parent[b] = a;
-    sz[a] += sz[b];
-}
-
-void init_union_find() {
-    for (int i=0; i<10000; i++) {
-        parent[i] = i;
-        sz[i] = 1;
-    }
-}
 
 int simulate() {
     int ret = 0;
     queue<pii> q;
     vector<vector<bool>> visited(N, vector<bool>(N, false));
     vector<vector<bool>> light_on(N, vector<bool>(N, false));
-    
-    init_union_find();
     
     q.push({0, 0});
     visited[0][0] = true;
@@ -48,39 +23,34 @@ int simulate() {
         int curr_c = q.front().second;
         q.pop();
         
+        // 스위치 켜기
         for (auto next_room: rooms[curr_r][curr_c]) {
-            // (curr_r, curr_c)에서 불 켤 수 있는 방
             int next_r = next_room.first;
             int next_c = next_room.second;
             int next_idx = next_r*N + next_c;
             
             if (!light_on[next_r][next_c]) ret++;
             light_on[next_r][next_c] = true;
-            
-            int dr[] = {-1, 1, 0, 0};
-            int dc[] = {0, 0, -1, 1};
-            
-            // 인접한 불켜진 방과 경로 연결
-            for (int i=0; i<4; i++) {
-                int adj_r = next_r + dr[i];
-                int adj_c = next_c + dc[i];
-                int adj_idx = adj_r*N + adj_c;
-                
-                if (adj_r < 0 || adj_r >= N || adj_c < 0 || adj_c >= N) continue;
-                
-                if (light_on[adj_r][adj_c]) {
-                    merge(next_idx, adj_idx);
-                }
-            }
         }
         
-        // (1,1)과 연결되어 있는 방 방문
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<N; j++) {
-                if (!light_on[i][j] || visited[i][j]) continue;
-                if (find(i*N+j) == find(0)) {
-                    q.push({i, j});
-                    visited[i][j] = true;
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+        
+        // (1,1)과 연결된 방 방문
+        for (int r=0; r<N; r++) {
+            for (int c=0; c<N; c++) {
+                if (!light_on[r][c] || visited[r][c]) continue;
+                
+                for (int i=0; i<4; i++) {
+                    int adj_r = r + dr[i];
+                    int adj_c = c + dc[i];
+                    
+                    if (adj_r < 0 || adj_r >= N || adj_c < 0 || adj_c >= N) continue;
+                    
+                    if (visited[adj_r][adj_c]) {
+                        visited[r][c] = true;
+                        q.push({r, c});
+                    }
                 }
             }
         }
