fd364cd31eafd4e47b3defd2ba65886d715b8621
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2022-09-18 23:33:24 +0900

    Update 17779.cpp

--- BOJ/17000-17999/17779.cpp
+++ BOJ/17000-17999/17779.cpp
@@ -8,7 +8,6 @@ const int INF = 987654321;
 int N;
 int A[20][20];
 int board[20][20];
-bool visit[20][20];
 
 bool validate(int x, int y) {
     return x >= 0 && x < N && y >= 0 && y < N;
@@ -30,7 +29,6 @@ void DFS(int r, int c) {
 
 int simulate(int x, int y, int d1, int d2) {
     memset(board, 0, sizeof(board));
-    memset(visit, 0, sizeof(visit));
     
     for (int i=0; i<=d1; i++) {
         if (!validate(x+i, y-i)) return INF;
@@ -59,10 +57,10 @@ int simulate(int x, int y, int d1, int d2) {
     for (int i=0; i<N; i++) {
         for (int j=0; j<N; j++) {
             if (board[i][j] == 5) pop_sum[5] += A[i][j];
-            else if (i < x+d1 && j <= y) {pop_sum[1] += A[i][j]; board[i][j]=1;}
-            else if (i <= x+d2 && y < j) {pop_sum[2] += A[i][j];board[i][j]=2;}
-            else if (x+d1 <= i && j < y-d1+d2) {pop_sum[3] += A[i][j];board[i][j]=3;}
-            else {pop_sum[4] += A[i][j]; board[i][j]=4;}
+            else if (i < x+d1 && j <= y) pop_sum[1] += A[i][j];
+            else if (i <= x+d2 && y < j) pop_sum[2] += A[i][j];
+            else if (x+d1 <= i && j < y-d1+d2) pop_sum[3] += A[i][j];
+            else pop_sum[4] += A[i][j];
         }
     }
     
