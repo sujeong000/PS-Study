cc529ba7685985600b08af2308f929c6b8f20431
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2022-10-07 02:13:24 +0900

    Update 1933.cpp

--- BOJ/1000-1999/1933.cpp
+++ BOJ/1000-1999/1933.cpp
@@ -1,3 +1,4 @@
+// sol 1. 우선순위큐
 #include <iostream>
 #include <queue>
 
@@ -69,6 +70,63 @@ int main()
     }
     
     ans.push_back({curr_r, 0});
+    
+    for (auto pos: ans) {
+        cout << pos.first << " " << pos.second << " ";
+    }
+
+    return 0;
+}
+
+// sol 2. map/set
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int N; cin >> N;
+    vector<piii> v;
+    for (int i=0; i<N; i++) {
+        int l, h, r;
+        cin >> l >> h >> r;
+        v.push_back({l, {0, -h}});
+        v.push_back({r, {1, h}});
+    }
+    
+    sort(v.begin(), v.end());
+    
+    vector<pii> ans;
+    map<int, int> mp;
+    for (auto pos: v) {
+        int x = pos.first;
+        int t = pos.second.first;
+        int h = t == 0 ? -pos.second.second : pos.second.second;
+        
+        // 시작점 or 끝 점에서만 높이가 변한다
+        if (t == 0) {   // 시작점
+            // 새 빌딩 or 더 높은 빌딩
+            if (mp.empty() || mp.rbegin()->first < h) ans.push_back({x, h});
+            mp[h]++;
+        }
+        else {          // 끝점
+            int max_h = mp.rbegin()->first;
+            mp[h]--;
+            if (mp[h] == 0) mp.erase(h);
+            
+            // 없어지면 땅 or 더 낮은 빌딩
+            if (mp.empty()) ans.push_back({x, 0});
+            else if (max_h != mp.rbegin()->first) ans.push_back({x, mp.rbegin()->first});
+        }
+    }
     
     for (auto pos: ans) {
         cout << pos.first << " " << pos.second << " ";
