63b5282c88fb788a26cab16199d0b577ad64496d
Sujeong Choi <70833900+sujeong000@users.noreply.github.com>
2023-07-24 17:59:10 +0900

    Update 14719.cpp

--- BOJ/14000-14999/14719.cpp
+++ BOJ/14000-14999/14719.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int H, W;
-bool board[501][501];
-
-int calcRain() {
-    int sum = 0;
+int calcRainwaterInColumn(int c, vector<int>& heights) {
+    vector<int>::iterator s = heights.begin();
+    vector<int>::iterator e = heights.end();
     
-    for (int i=0; i<H; i++) {
-        int last = -1;
-        
-        for (int j=0; j<W; j++) {
-            if (board[i][j]) {
-                if (last != -1) sum += j - last - 1;
-                last = j;
-            }
-        }
+    // 둘러싸고 있는 양쪽 블록 중 각각 가장 높은 블록을 구함
+    int left_max = *max_element(s, s + c);
+    int right_max = *max_element(s + c + 1, e);
+    
+    // 그 중 더 낮은 블록 높이까지 빗물이 고임
+    int limit = min(left_max, right_max);
+    int accumulated_rainwater = max(0, limit - heights[c]);
+    
+    return accumulated_rainwater;
+}
+
+int solution(int H, int W, vector<int>& heights) {
+    int answer = 0;
+    
+    // 양 끝 열에는 고일 수 없음
+    for (int i = 1; i < (W - 1); i++) {
+        answer += calcRainwaterInColumn(i, heights);
     }
     
-    return sum;
+    return answer;
 }
 
 int main()
@@ -27,18 +35,16 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
-    cin >> H >> W;
+    int H, W;
+    vector<int> heights;
     
-    for (int j=0; j<W; j++) {
-        int h;
-        cin >> h;
-        
-        for (int i=0; i<h; i++) {
-            board[i][j] = true;
-        }
+    cin >> H >> W;
+    heights.resize(W);
+    for (int i = 0; i < W; i++) {
+        cin >> heights[i];
     }
     
-    cout << calcRain();
+    cout << solution(H, W, heights);
 
     return 0;
 }
